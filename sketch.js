5a8b6ef6261596b43f470bec2cfb85be22dba108
TunmiseCroissant <tunmifolorunsho@gmail.com>
2026-02-27 23:10:00 -0600

    basic accleration due to gravity for a block but currently no colissions

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+let mass1;
+let a = -0.981;
+
 function setup() {
     createCanvas(800, 600);
+    background("white");
+    mass1 = new body(200, 500, 100, 100);
+    
 }
 
 function draw() {
-    background(220);
+    background("white");
+    fill("green")
+    rect(0, height - 30, width, 30)
+    mass1.show();
+    mass1.gravity();
+}
+
+class body {
+    constructor(x, y, w, h, mass) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.mass = mass;
+        this.v = 0;
+    }
+
+    show() {
+        fill("pink")
+        stroke("black")
+        strokeWeight(2)
+        rect(this.x, height - this.y, this.w, this.h)
+    }
+
+    gravity() {
+        this.v += a
+        this.y += this.v
+    }
 }
