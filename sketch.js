4c3c16a646d1457097f6b76c799e2145ecca7883
TunmiseCroissant <tunmifolorunsho@gmail.com>
2026-02-28 00:01:44 -0600

    added basic physics so that velocity and accleration make the object move accurately

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
 let mass1;
-let a = -0.981;
+let g = -0.981;
+let floor;
+let words = ""
 
 function setup() {
     createCanvas(800, 600);
     background("white");
-    mass1 = new body(200, 500, 100, 100);
+    mass1 = new body(100, 200, 100, 100, 1, g, 20);
+    floor = new ground(30)
     
 }
 
 function draw() {
     background("white");
-    fill("green")
-    rect(0, height - 30, width, 30)
+    floor.show();
+    fill("black")
+    strokeWeight(0)
+    text(words, 600, 100);
     mass1.show();
     mass1.gravity();
 }
 
+class ground {
+    constructor(h) {
+        this.h = h;
+        this.y = height - this.h;
+    }
+
+    show() {
+        fill("green")
+        rect(0, this.y, width, this.h)
+    }
+}
+
 class body {
-    constructor(x, y, w, h, mass) {
+    constructor(x, y, w, h, mass, a = g, v =0) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.mass = mass;
-        this.v = 0;
+        this.v = v;
+        this.a = a;
+        this.f = this.mass * g;
+        this.NF = this.f;
+        this.vx = 10;
     }
 
     show() {
@@ -34,7 +55,17 @@ class body {
     }
 
     gravity() {
-        this.v += a
+        this.a = g * this.mass;
+        this.v += this.a
+        if (((this.y - this.h) + this.v) <= floor.h) {
+            this.v = 0;
+            this.a = 0;
+            this.y = floor.h + this.h
+            this.vx = 0;
+        }
         this.y += this.v
+        this.NF = this.mass * this.a;
+        this.x += this.vx;
+        words = `speed = ${this.v.toFixed(2)} m/s`
     }
 }
